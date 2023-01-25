fa57d0756b94f02e93aeb3e65edb8a6e8572444f
unknown <kkishimoto@village88.com>
2023-01-25 15:33:54 +0800

    added minor changes

--- web_frontend/assets/js/nav.js
+++ web_frontend/assets/js/nav.js
@@ -1,18 +1,8 @@
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+
     var select = document.querySelectorAll('select');
+    var dropdown = M.FormSelect.init(select);
   });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-     $('select').formSelect();
-     $('#mobile_nav').sidenav();
-  });
-        
