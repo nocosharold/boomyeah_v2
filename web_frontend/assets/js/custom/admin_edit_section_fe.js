f43a14af1d68886e89f61fbe44fc3523ebe72544
Jan Dreau Ganggangan <v88jadee@Jans-MacBook-Air.local>
2023-01-27 13:59:15 +0800

    Fix issue with section tabs (remove, update); Fix style of Allow Comments checkbox

--- web_frontend/assets/js/custom/admin_edit_section_fe.js
+++ web_frontend/assets/js/custom/admin_edit_section_fe.js
@@ -1,171 +1,180 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    ux("#add_page_tabs_btn").on("click", addNewSectionContent);
-    initializeEditSectionEvents();
-    initializeRedactor("#section_pages .tab_content");
-});
+(
+function(){
+    let toast_timeout = null;
+    let saving_timeout = null;
 
-function initializeEditSectionEvents(ux_target = null, callback = null){
-    if(ux_target){
-        ux_target.find(".section_page_tabs .add_page_btn").on("click", addNewTab);
-        ux_target.find(".section_page_tabs .remove_tab_btn").on("click", removeSectionTab);
-        bindOpenTabLink(ux_target);
-
-        ux_target.findAll((".tab_title")).forEach((tab_title, tab_index) => {
-            ux(tab_title).on("keyup", (event) => {
-                onUpdateTabTitle(event, tab_index + 1);
-            });
-        });
-    }
-    else{
-        ux(".section_page_tabs .add_page_btn").onEach("click", addNewTab);
-        ux(".section_page_tabs .remove_tab_btn").onEach("click", removeSectionTab);
-        ux(".section_page_content .tab_title").onEach("keyup", (event, tab_index) => {
-            onUpdateTabTitle(event, tab_index);
-        });
-        bindOpenTabLink();
-    }
-
-    if(callback){
-        callback();
-    }
-}
-let toast_timeout = null;
-let saving_timeout = null;
-function saveTabChanges(section_page_tab){
-    clearTimeout(saving_timeout);
-    M.Toast.dismissAll();
+    document.addEventListener("DOMContentLoaded", ()=>{
+        ux("#add_page_tabs_btn").on("click", addNewSectionContent);
+        initializeEditSectionEvents();
+        initializeRedactor("#section_pages .tab_content");
+    });
     
-    saving_timeout = setTimeout(() => {        
-        clearTimeout(toast_timeout);
-        section_page_tab.find(".saving_indicator").addClass("show");
+    function initializeEditSectionEvents(ux_target = null, callback = null){
+        if(ux_target){
+            ux_target.find(".section_page_tabs .add_page_btn").on("click", addNewTab);
+            ux_target.find(".section_page_tabs .remove_tab_btn").on("click", removeSectionTab);
+            bindOpenTabLink(ux_target);
     
-        toast_timeout = setTimeout(() => {
-            section_page_tab.find(".saving_indicator").removeClass("show");
-            M.toast({
-                html: "Changes Saved",
-                displayLength: 2800
+            ux_target.findAll((".tab_title")).forEach((tab_title) => {
+                ux(tab_title).on("keyup", (event) => {
+                    onUpdateTabTitle(event);
+                });
             });
+        }
+        else{
+            ux(".section_page_tabs .add_page_btn").onEach("click", addNewTab);
+            ux(".section_page_tabs .remove_tab_btn").onEach("click", removeSectionTab);
+            ux(".section_page_content .tab_title").onEach("keyup", (event) => {
+                onUpdateTabTitle(event);
+            });
+            bindOpenTabLink();
+        }
+    
+        if(callback){
+            callback();
+        }
+    }
+    
+    function saveTabChanges(section_page_tab){
+        clearTimeout(saving_timeout);
+        M.Toast.dismissAll();
+        
+        saving_timeout = setTimeout(() => {        
+            clearTimeout(toast_timeout);
+            section_page_tab.find(".saving_indicator").addClass("show");
+        
+            toast_timeout = setTimeout(() => {
+                section_page_tab.find(".saving_indicator").removeClass("show");
+                M.toast({
+                    html: "Changes Saved",
+                    displayLength: 2800,
+                });
+                
+            }, 800);
+        }, 480);
+    }
+    
+    function onUpdateTabTitle(event){
+        let tab_title = event.target;
+        let section_page_tab = ux(tab_title.closest(".section_page_tab"));
+        let tab_id = section_page_tab.attr("id");
+        let tab_title_value = (tab_title.value.length > 0) ? tab_title.value : "Untitled Tab*";
+        ux(`.page_tab_item[data-tab_id="${tab_id}"] a`).text(tab_title_value);
+        
+        if(tab_title.value.length > 0){
+            saveTabChanges(section_page_tab);
+        }
+    }
+    
+    function bindOpenTabLink(ux_target = null){
+        if(ux_target){
+            /** For dynamically added sections */
+            ux_target.findAll((".section_page_tabs .page_tab_item")).forEach((page_tab_link) => {
+                ux(page_tab_link).on("click", (link_event) => {
+                    openTabLink(link_event);
+                });
+            })
+        } else {
             
-        }, 800);
-    }, 480);
-}
-
-function onUpdateTabTitle(event, tab_index){
-    let tab_title = event.target;
-    let section_page_tab = ux(tab_title.closest(".section_page_tab"));
-    let section_page_content = ux(tab_title.closest(".section_page_content"));
-    let section_page_tabs_list = section_page_content.find(".section_page_tabs");
-    section_page_tabs_list.find(`.page_tab_item:nth-child(${tab_index}) a`).text(tab_title.value);
-    saveTabChanges(section_page_tab);
-}
-
-function bindOpenTabLink(ux_target = null){
-    if(ux_target){
-        /** For dynamically added sections */
-        ux_target.findAll((".section_page_tabs .page_tab_item")).forEach((page_tab_link, tab_index) => {
-            ux(page_tab_link).on("click", (link_event) => {
-                openTabLink(link_event, tab_index + 1);
+            ux(".section_page_tabs .page_tab_item").onEach("click", (event) =>{
+                openTabLink(event);
             });
-        })
-    } else {
+        }
+    }
+    
+    function openTabLink(event){
+        let tab_item = event.target;
+        let section_page_content = ux(tab_item.closest(".section_page_content"));
+        let section_page_tabs_list = ux(tab_item.closest(".section_page_tabs"));
+        let page_tab_item = tab_item.closest(".page_tab_item");
+        let tab_id = ux(page_tab_item).attr("data-tab_id");
+    
+        section_page_tabs_list.findAll(".page_tab_item").forEach(element => element.classList.remove("active"))
+        section_page_content.findAll(".section_page_tab").forEach(element => element.classList.remove("show"))
         
-        ux(".section_page_tabs .page_tab_item").onEach("click", (event, tab_index) =>{
-            openTabLink(event, tab_index);
+        setTimeout(() => {
+            ux(page_tab_item).addClass("active");
+            ux(`#${ tab_id }`).addClass("show")
+                .find(".tab_title").html().select();
         });
     }
-}
-
-function openTabLink(event, tab_index){
-    let tab_item = event.target;
-    let section_page_content = ux(tab_item.closest(".section_page_content"));
-    let section_page_tabs_list = ux(tab_item.closest(".section_page_tabs"));
-
-    section_page_tabs_list.findAll(".page_tab_item").forEach(element => element.classList.remove("active"))
-    section_page_content.findAll(".section_page_tab").forEach(element => element.classList.remove("show"))
     
-    setTimeout(() => {
-        section_page_tabs_list.find(`.page_tab_item:nth-child(${tab_index})`).addClass("active");
-        section_page_content.find(`.section_page_tab:nth-child(${tab_index + 1})`).addClass("show")
-            .find(".tab_title").html().select();
-    });
-}
-
-function addNewSectionContent(event){
-    event.preventDefault();
-    let tab_id = `tab_${ new Date().getTime()}`;
-    let section_pages = ux("#section_pages");
-    let section_page_content = ux("#clone_section_page .section_page_content").clone();
-    let section_page_tab = section_page_content.find(".section_page_tab");
-    section_page_content.find(".page_tab_item").addClass("active");
-    section_page_tab.addClass("show");
-    section_pages.html().append(section_page_content.html());
-    section_page_tab.html().id = tab_id;
-    section_page_content.find(".section_page_tab .tab_title").html().select();
-    section_page_content.find(".section_page_tabs .page_tab_item").html()
-        .setAttribute("data-tab_id", tab_id);
-
-    /** Rebind Event Listeners */
-    initializeEditSectionEvents(section_page_content);
-    initializeRedactor(`#${tab_id} .tab_content`);
-}
-
-function addNewTab(event){
-    event.preventDefault();
-    let tab_item = event.target;
-    let add_page_tab = event.target.closest(".add_page_tab");
-    let section_page_content = ux(tab_item.closest(".section_page_content"));
-    let section_page_tabs_list = ux(tab_item.closest(".section_page_tabs"));
-    let page_tab_clone = ux("#clone_section_page .section_page_tab").clone();
-    let page_tab_item = ux("#clone_section_page .page_tab_item").clone();
-    let tab_id = `tab_${ new Date().getTime()}`;
-    page_tab_clone.html().id = tab_id;
-    section_page_content.html().append(page_tab_clone.html());
+    function addNewSectionContent(event){
+        event.preventDefault();
+        let tab_id = `tab_${ new Date().getTime()}`;
+        let section_pages = ux("#section_pages");
+        let section_page_content = ux("#clone_section_page .section_page_content").clone();
+        let section_page_tab = section_page_content.find(".section_page_tab");
+        section_page_content.find(".page_tab_item").addClass("active");
+        section_page_tab.addClass("show");
+        section_pages.html().append(section_page_content.html());
+        section_page_tab.html().id = tab_id;
+        section_page_content.find(".section_page_tab .tab_title").html().select();
+        section_page_content.find(".section_page_tabs .page_tab_item").html()
+            .setAttribute("data-tab_id", tab_id);
     
-    /** Insert New tab */
-    section_page_tabs_list.html().append(page_tab_item.html());
-    section_page_tabs_list.html().append(add_page_tab);
-    page_tab_item.find(".remove_tab_btn").on("click", removeSectionTab);
-    
-    page_tab_clone.find(".tab_title").on("keyup", (event) => {
-        onUpdateTabTitle(event, section_page_tabs_list.findAll(".page_tab_item").length);
-    });
-    page_tab_item.html().setAttribute("data-tab_id", tab_id);
-    
-    bindOpenTabLink(section_page_content);
-    
-    setTimeout(() => {
+        /** Rebind Event Listeners */
+        initializeEditSectionEvents(section_page_content);
         initializeRedactor(`#${tab_id} .tab_content`);
+    }
+    
+    function addNewTab(event){
+        event.preventDefault();
+        let tab_item = event.target;
+        let add_page_tab = event.target.closest(".add_page_tab");
+        let section_page_content = ux(tab_item.closest(".section_page_content"));
+        let section_page_tabs_list = ux(tab_item.closest(".section_page_tabs"));
+        let page_tab_clone = ux("#clone_section_page .section_page_tab").clone();
+        let page_tab_item = ux("#clone_section_page .page_tab_item").clone();
+        let tab_id = `tab_${ new Date().getTime()}`;
+        page_tab_clone.html().id = tab_id;
+        section_page_content.html().append(page_tab_clone.html());
         
-        /** Auto click new tab */            
-        page_tab_item.html().click();
-    });
-}
-
-function removeSectionTab(event){
-    event.stopPropagation();
-
-    let remove_tab_btn = event.target;
-    let tab_item = remove_tab_btn.closest(".page_tab_item");
-    let section_page_content = remove_tab_btn.closest(".section_page_content");
-    let section_page_tabs = remove_tab_btn.closest(".section_page_tabs");
-    let tab_id = tab_item.getAttribute("data-tab_id");
+        /** Insert New tab */
+        section_page_tabs_list.html().append(page_tab_item.html());
+        section_page_tabs_list.html().append(add_page_tab);
+        page_tab_item.html().setAttribute("data-tab_id", tab_id);
+        
+        page_tab_item.find(".remove_tab_btn").on("click", removeSectionTab);
+        page_tab_clone.find(".tab_title").on("keyup", (event) => {
+            onUpdateTabTitle(event, section_page_tabs_list.findAll(".page_tab_item").length);
+        });
+        bindOpenTabLink(section_page_content);
+        
+        setTimeout(() => {
+            initializeRedactor(`#${tab_id} .tab_content`);
+            
+            /** Auto click new tab */            
+            page_tab_item.html().click();
+        });
+    }
     
-    ux(`#${tab_id}`).html().remove();
-    tab_item.remove();
+    function removeSectionTab(event){
+        event.stopPropagation();
     
-    setTimeout(() => {
-        if(ux(section_page_tabs).findAll(".page_tab_item").length === 0){
-            section_page_content.remove();
-        }else{
-            ux(section_page_tabs).findAll(".page_tab_item")[0].click();
-        }
-    });
-}
-
-function initializeRedactor(selector){
-    RedactorX(selector, {
-        editor: {
-            minHeight: '360px'
-        }
-    });
-}
+        let remove_tab_btn = event.target;
+        let tab_item = remove_tab_btn.closest(".page_tab_item");
+        let section_page_content = remove_tab_btn.closest(".section_page_content");
+        let section_page_tabs = remove_tab_btn.closest(".section_page_tabs");
+        let tab_id = tab_item.getAttribute("data-tab_id");
+        
+        ux(`#${tab_id}`).html().remove();
+        tab_item.remove();
+        
+        setTimeout(() => {
+            if(ux(section_page_tabs).findAll(".page_tab_item").length === 0){
+                section_page_content.remove();
+            }else{
+                ux(section_page_tabs).findAll(".page_tab_item")[0].click();
+            }
+        });
+    }
+    
+    function initializeRedactor(selector){
+        RedactorX(selector, {
+            editor: {
+                minHeight: '360px'
+            }
+        });
+    }
+})();
