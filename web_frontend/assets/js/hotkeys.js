56eee626679154a53df4ff2ec740729502b89eaf
Jovic Abengona <jvlabengona@gmail.com>
2023-02-01 09:50:36 +0800

    Add condition to identify if user is admin or user

--- web_frontend/assets/js/hotkeys.js
+++ web_frontend/assets/js/hotkeys.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function checkShortcut(e){
+    const current_path = window.location.pathname;
+    const user_type = current_path.includes("admin") ? "admin" : "user";
     const SHORTCUT_VALUES = {
-        "keyCode_49_url": "/views/user_documentation",
-        "keyCode_50_url": "/views/no_data/user_documentation",
-        "keyCode_51_url": "/views/small_data/user_documentation",
-        "keyCode_52_url": "/views/medium_data/user_documentation",
-        "keyCode_53_url": "/views/large_data/user_documentation"
+        "keyCode_49_url": `/views/${user_type}_documentation`,
+        "keyCode_50_url": `/views/no_data/${user_type}_documentation`,
+        "keyCode_51_url": `/views/small_data/${user_type}_documentation`,
+        "keyCode_52_url": `/views/medium_data/${user_type}_documentation`,
+        "keyCode_53_url": `/views/large_data/${user_type}_documentation`
     }
 
     let shortcut_event = window.event? event : e;
