57e0f6403ddc4883973eb89dafed7237f3197f35
unknown <kkishimoto@village88.com>
2023-02-13 11:20:29 +0800

    user view doc

--- web_frontend/assets/js/user_view_documentation.js
+++ web_frontend/assets/js/user_view_documentation.js
@@ -14,6 +14,9 @@ document.addEventListener("DOMContentLoaded", async () => {
         location.href = "user_view_section";
     });
 
+    ux(".sort_by").onEach("click", sort_documentations);
+    initializeMaterializeDropdown();
+
     /* Print JSON data */
     // printSectionOutline(data.section_outline.sections);
 }); 
@@ -34,6 +37,31 @@ function printSectionOutline(section_data){
                 </div>
             </div>`;
     });
+}
 
-    console.log(section_block);
+
+function sort_documentations(event){
+    let sort_by = ux(event.target).attr("data-sort-by");
+    let section_lists = document.getElementById('section_container');
+    let section_list_nodes = section_lists.childNodes;
+    let section_lists_to_sort = [];
+
+    for (let i in section_list_nodes) {
+        (section_list_nodes[i].nodeType == 1) && section_lists_to_sort.push(section_list_nodes[i]);
+    }
+    
+    section_lists_to_sort.sort(function(a, b) {
+        return a.innerHTML == b.innerHTML ? 0 : ( sort_by === "az" ? (a.innerHTML > b.innerHTML ? 1 : -1) : (b.innerHTML > a.innerHTML ? 1 : -1) );
+    });
+
+    for (let i = 0; i < section_lists_to_sort.length; ++i) {
+        section_lists.appendChild(section_lists_to_sort[i]);
+    }
+}
+
+function initializeMaterializeDropdown(){
+    const elems = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(elems, {
+        coverTrigger: false
+    });
 }
