316517de5a4c0af197e25109908a4af4d3f7136b
Jan Dreau Ganggangan <v88jadee@Jans-MacBook-Air.local>
2023-02-14 15:57:28 +0800

    Refactor main navigation codes

--- web_frontend/assets/js/main_navigation.js
+++ web_frontend/assets/js/main_navigation.js
@@ -4,67 +4,49 @@ let select = document.querySelectorAll('select');
 let dropdown = M.FormSelect.init(select);
 
 ux("#search_documentation_field").on("keydown", onSearchDocumentation);
-ux("#search_section_field").on("keydown", onSearchSection);
+ux("#search_section_field").on("keyup", onSearchSection);
 
 let search_timeout = null;
 function onSearchDocumentation(event){
     clearTimeout(search_timeout);
-
+    let search_field = event.target;
+    let search_value = search_field.value.toLowerCase();
+    
     search_timeout = setTimeout(() => {
-        let search_field = event.target;
-        let search_value = search_field.value.toLowerCase();
-        let matched_elements = [];
-        
-        if(search_value.trim().length){
-            ux("#documentations").findAll(".document_block .document_details h2").forEach((search_element) => {
-                let matching_value = search_element.innerText.toLowerCase();
-                let document_block = search_element.closest(".document_block");
-                ux(document_block).addClass("hidden");
-
-                if(matching_value.indexOf(search_value) > -1){
-                    matched_elements.push(document_block);
-                }
-            });
-
-            if(matched_elements.length){
-                matched_elements.forEach((matched_element) => ux(matched_element).removeClass("hidden"));
-                return;
-            }
-        }
-
-        ux("#documentations").findAll(".document_block").forEach((document_block) => {
-            ux(document_block).removeClass("hidden");
-        });
+        onSearchViewItem(ux("#documentations"), search_value, "innerText", ".document_block", ".document_block .document_details h2");
     }, 480);
 }
 
 function onSearchSection(event){
     clearTimeout(search_timeout);
+    let search_field = event.target;
+    let search_value = search_field.value.toLowerCase();
 
     search_timeout = setTimeout(() => {
-        let search_field = event.target;
-        let search_value = search_field.value.toLowerCase();
-        let matched_elements = [];
-        
-        if(search_value.trim().length){
-            ux("#section_container").findAll(".section_block .section_details .section_title").forEach((search_element) => {
-                let matching_value = search_element.value.toLowerCase();
-                let section_block = search_element.closest(".section_block");
-                ux(section_block).addClass("hidden");
-
-                if(matching_value.indexOf(search_value) > -1){
-                    matched_elements.push(section_block);
-                }
-            });
-
-            if(matched_elements.length){
-                matched_elements.forEach((matched_element) => ux(matched_element).removeClass("hidden"));
-                return;
-            }
-        }
-
-        ux("#section_container").findAll(".section_block").forEach((section_block) => {
-            ux(section_block).removeClass("hidden");
-        });
+        onSearchViewItem(ux("#section_container"), search_value, "value", ".section_block", ".section_block .section_details .section_title")
     }, 480);
+}
+
+function onSearchViewItem(target_container, search_value = "", matching_field = "innerText", target_element, target_item){
+    target_container.findAll(target_element).forEach((section_block) => {
+        ux(section_block).removeClass("hidden");
+    });
+    
+    let matched_elements = [];
+    if(search_value.trim().length){
+        target_container.findAll(target_item).forEach((search_element) => {
+            let matching_value = search_element[matching_field].toLowerCase();
+            let section_block = search_element.closest(target_element);
+            ux(section_block).addClass("hidden");
+
+            if(matching_value.indexOf(search_value) > -1){
+                matched_elements.push(section_block);
+            }
+        });
+
+        if(matched_elements.length){
+            matched_elements.forEach((matched_element) => ux(matched_element).removeClass("hidden"));
+            return;
+        }
+    }
 }
